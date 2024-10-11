f16147e7d57acaa761a2bf50bae778f1aef8a9d7
Antti Romppanen <romppanen.antti@gmail.com>
2024-10-11 08:07:21 +0300

    added css properties object

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -7,3 +7,9 @@ export function errorText(text: string) {
 export default function printHeader(text: string) {
   console.log(chalk.bold.cyan(`\n* ${text} *\n`));
 }
+
+export function cssPropertyNotFoundText() {
+  errorText(
+    "CSS property not found in Tailwind CSS. Try different keyword (i.e. backdrop-blur instead of filter-backdrop-blur, or scale instead of transform-scale).",
+  );
+}
